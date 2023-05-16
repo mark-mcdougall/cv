a13abcb5cf1d917991667e901fef92dbb034fe53
Mark McDougall <mark.mcdougall@hotmail.co.uk>
2023-05-16 02:20:43 +0100

    update icons and styling

--- project/app.py
+++ project/app.py
@@ -62,10 +62,10 @@ st.write("#")
 st.subheader("Experience and Qualifications")
 st.write(
     """
-    - 8 years experience extracting actionable insights from data
-    - Strong knowledge and hands-on experience in data analysis using Python and SQL
-    - 3 years experience working remotely with international teams in multiple time zones
-    - Experience leading projects in an agile environment and mentoring junior team members
+    - ✅ 8 years experience extracting actionable insights from data
+    - ✅ Strong knowledge and hands-on experience in data analysis using Python and SQL
+    - ✅ Experience leading projects in an agile environment and mentoring junior team members
+    - ✅ 3 years experience working remotely with international teams in multiple time zones
     """
 )
 
@@ -74,15 +74,15 @@ st.write("#")
 st.subheader("Skills")
 st.write(
     """
-    - Data Analysis: Python (Pandas, Numpy, Scikit-learn), SQL
-    - Data Visualisation: Python (Holoviz, Plotly, hvplot, Panel, Matplotlib, Seaborn)
-    - Geospatial Data Analysis and Mapping: Python (GeoPandas, Geoviews, Shapely, Cartopy)
-    - Data Pipelines: Python (Pandas, Numpy, SQLAlchemy, Requests), SQL
-    - Databases: PostgreSQL, SQLite, MS SQL Server
-    - Containerisation: Docker
-    - Version Control: Git, GitHub
-    - Agile: Jira, Confluence
-    - Customer Interaction: Salesforce
+    - 🧮 Data Analysis: Python (Pandas, Pandas API on Spark, Scikit-learn), SQL
+    - 📊 Data Visualisation: Python (Holoviz, Plotly, hvplot, Panel, Matplotlib, Seaborn)
+    - 🗺️ Geospatial Data Analysis and Mapping: Python (GeoPandas, Geoviews, Shapely, Cartopy)
+    - ➖ Data Pipelines: Python (Pandas, Numpy, SQLAlchemy, Requests), SQL
+    - 🗄️ Databases: PostgreSQL, SQLite, MS SQL Server
+    - ⭕ Containerisation: Docker
+    - ⭕ Version Control: Git, GitHub
+    - ⭕ Agile: Jira, Confluence
+    - ⭕ Customer Interaction: Salesforce
     """
 )
 
@@ -96,11 +96,11 @@ st.write("**Data Analyst | S and P Global Platts**")
 st.write("September 2021 - March 2023")
 st.write(
     """
-    * Supported two development teams in the migration of approximately 150 datasets to DataBricks through end-to-end analysis of existing dataflows
-    * Used Python, SQL and DataBricks to produce an automated, twice daily email report that alerted the Data Collections team to any issues with datasets missing data
-    * Produced a workflow in Python that allowed external customers to access and process data using an api
-    * Performed software releases of the BAU team’s data fixes to the Production environment
-    * Greatly improved the onboarding of new BAU team members by identifying current processes and writing documentation
+    * ▶️ Supported two development teams in the migration of approximately 150 datasets to DataBricks through end-to-end analysis of existing dataflows
+    * ▶️ Used Python, SQL and DataBricks to produce an automated, twice daily email report that alerted the Data Collections team to any issues with datasets missing data
+    * ▶️ Produced a workflow in Python that allowed external customers to access and process data using an api
+    * ▶️ Performed software releases of the BAU team’s data fixes to the Production environment
+    * ▶️ Greatly improved the onboarding of new BAU team members by identifying current processes and writing documentation
     """
 )
 
@@ -109,10 +109,10 @@ st.write("**Data Analyst | B and PO**")
 st.write("April 2019 - March 2021")
 st.write(
     """
-    * Delivered bespoke analytics reports for multinational, household name clients
-    * Produced interactive presales reports to assist the sales team in winning new business
-    * Delivered a new geospatial analytics method that resulted in multiple new clients and tens of thousands of pounds in revenue
-    * Sourced, cleaned and transformed freely available online datasets from multiple reputable sources to produce richer, original datasets with actionable business value, saving my employer tens of thousands annually in data costs
+    * ▶️ Delivered bespoke analytics reports for multinational, household name clients
+    * ▶️ Produced interactive presales reports to assist the sales team in winning new business
+    * ▶️ Delivered a new geospatial analytics method that resulted in multiple new clients and tens of thousands of pounds in revenue
+    * ▶️ Sourced, cleaned and transformed freely available online datasets from multiple reputable sources to produce richer, original datasets with actionable business value, saving my employer tens of thousands annually in data costs
     """
 )
 
@@ -121,10 +121,10 @@ st.write("**Data Analyst | Adapptive**")
 st.write("April 2017 - April 2019")
 st.write(
     """
-    * In collaboration with four developers, led the successful delivery of multiple new features and the full redesign of the existing functionality of the Adapptive advertising software product
-    * Produced mock data datasets to enable the development, testing and demonstration of new software features
-    * Managed a team of four developers and a data analyst when my boss was frequently working remotely at another office
-    * Recruited, trained and mentored a new data analyst
+    * ▶️ In collaboration with four developers, led the successful delivery of multiple new features and the full redesign of the existing functionality of the Adapptive advertising software product
+    * ▶️ Produced mock data datasets to enable the development, testing and demonstration of new software features
+    * ▶️ Managed a team of four developers and a data analyst when my boss was frequently working remotely at another office
+    * ▶️ Recruited, trained and mentored a new data analyst
     """
 )
 
@@ -133,8 +133,8 @@ st.write("**Data Analyst | Red Fox Media**")
 st.write("January 2015 - April 2017")
 st.write(
     """
-    * Designed, trafficked, managed and optimized ad campaigns, including the production of customer data segments
-    * Cleaned, processed, updated and managed client data files and third-party data sources
+    * ▶️ Designed, trafficked, managed and optimized ad campaigns, including the production of customer data segments
+    * ▶️ Cleaned, processed, updated and managed client data files and third-party data sources
     """
 )
 
